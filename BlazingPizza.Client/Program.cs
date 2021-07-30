aaa40ecd68aa43d2b93c1a3a82841a906b5b3652
sabelo <ntabenism@gmail.com>
2021-07-30 09:07:18 +0200

    Add Pizza Auth state to persist

--- BlazingPizza.Client/Program.cs
+++ BlazingPizza.Client/Program.cs
@@ -16,7 +16,8 @@ namespace BlazingPizza.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<OrderState>();
-            builder.Services.AddApiAuthorization();
+            builder.Services.AddApiAuthorization<PizzaAuthenticationState>();
+            // builder.Services.AddApiAuthorization();
             builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
