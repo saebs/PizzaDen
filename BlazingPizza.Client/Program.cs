5f51cd9e74593f4f0bbf0c4ccd318d52c7037b27
sabelo <ntabenism@gmail.com>
2021-07-30 09:24:52 +0200

    redirect to homepage on logout

--- BlazingPizza.Client/Program.cs
+++ BlazingPizza.Client/Program.cs
@@ -16,8 +16,12 @@ namespace BlazingPizza.Client
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddScoped<OrderState>();
-            builder.Services.AddApiAuthorization<PizzaAuthenticationState>();
-            // builder.Services.AddApiAuthorization();
+            // Add auth services
+            builder.Services.AddApiAuthorization<PizzaAuthenticationState>(options =>
+            {
+                options.AuthenticationPaths.LogOutSucceededPath = "";
+            });
+
             builder.Services.AddHttpClient<OrdersClient>(client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
                 .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
 
